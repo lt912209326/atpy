40f1f93ea46b22c0ce5d94d5b5af983c54a61e17
lt912209326 <54204819+lt912209326@users.noreply.github.com>
2020-04-18 16:20:13 +0900

    Add files via upload

---
+++ setup2.0.0.py
@@ -0,0 +1,38 @@
+# from distutils.core import setup, Extension
+from setuptools import setup, Extension,find_packages
+from Cython.Build import cythonize
+from Cython.Distutils import build_ext
+
+
+extension = [Extension("atpy.src.cython.elements",
+                    ['src/cython/elements.pyx'],
+                    include_dirs = ["./core"],
+                    language="c++",
+                    extra_compile_args = ["-O3", "-Wall"]),
+            # Extension("atpy.src.cppelement",
+                    # ['src/cppelement.pyx'],
+                    # include_dirs = ["./src"],
+                    # language="c++",
+                    # extra_compile_args = ["-O3", "-Wall"]),
+            ]
+
+setup(name='atpy',
+      packages=find_packages(),
+      cmdclass = {'build_ext': build_ext},
+      ext_modules=cythonize(extension,compiler_directives={'language_level':3}),
+      package_dir={'':'..'},
+      # packages = ['atpy.src.cpp','atpy.src.cython']
+      # package_data={'./include': ['cppelement.pxd']},
+      # include_path=['.core','.src'],
+      # compiler_directives={'language_level':3},
+      # cythonize(extension,
+                            # compiler_directives={'language_level':3},
+                            # include_path=['./core','../include'])
+     )
+# setup(
+    # name='my_cython_package',
+    # packages=['core'],
+    # package_data={'core': ['cppelement.pxd']},  # or wherever the files are
+    # ...etc...
+
+                            # package_data=['../include':'cppelement.pxd'])
